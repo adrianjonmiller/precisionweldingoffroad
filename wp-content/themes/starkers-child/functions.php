37a762fc0fecd38e4514baa44a329b96eeb6202e
Adrian Jon Miller <adrianjonmiller@gmail.com>
2013-04-30 14:42:36 -0700

    IE CSS Fixes

--- wp-content/themes/starkers-child/functions.php
+++ wp-content/themes/starkers-child/functions.php
@@ -32,7 +32,7 @@
 		wp_register_script( 'flexslider', get_stylesheet_directory_uri().'/js/jquery.flexslider-min.js', array( 'jquery' ),'', true );
 		wp_enqueue_script( 'flexslider' );
 		
-		wp_register_script( 'modernizr', get_stylesheet_directory_uri().'/js/modernizr.custom.45125.js', array( 'jquery' ),'', true );
+		wp_register_script( 'modernizr', get_stylesheet_directory_uri().'/js/modernizr.custom.45125.js', array( 'jquery' ),'', false );
 		wp_enqueue_script( 'modernizr' );
 		
 //		wp_register_script( 'backgroundPosition', get_stylesheet_directory_uri().'/js/jquery.backgroundPosition.js', array( 'jquery' ),'', true );
